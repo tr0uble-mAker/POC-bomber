dcb296cc9142a927bc39e72e69654cddd5ab4e26
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-11-30 20:56:14 +0800

    Delete pocs/middleware/jboss directory

--- pocs/middleware/jboss/main.py
+++
@@ -1,8 +0,0 @@
-from pocs.middleware.jboss.CVE_2017_12149 import CVE_2017_12149
-
-def jboss(url):          # 返回poc检测函数字符串列表
-    poclist = [
-        'CVE_2017_12149("{0}")'.format(url),
-
-    ]
-    return poclist

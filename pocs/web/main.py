b885442c3479bdb5d0809eb80095c18fd8297294
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-02 21:38:02 +0800

    Update main.py

--- pocs/web/main.py
+++ pocs/web/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 from pocs.web.weaver.main import *
+from pocs.web.seeyon.main import *
 from pocs.web.CVE_2021_22205 import CVE_2021_22205
 from pocs.web.ueditor_1433_parsing_vulnerabilitly import ueditor_1433_Parsing_vulnerability
 from pocs.web.CVE_2021_21972 import CVE_2021_21972
@@ -10,6 +11,7 @@ def web(url):
     print('\n[+] 正在加载 常见Web漏洞 poc检测模块......')
     poclist = []
     poclist = poclist + weaver(url)
+    poclist = poclist + seeyon(url)
     poclist = poclist + [
         'CVE_2021_22205("{0}")'.format(url),
         'CVE_2021_21972("{0}")'.format(url),

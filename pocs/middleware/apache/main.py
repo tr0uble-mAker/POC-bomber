f2da212686afbe9afb807a569deef866fdebbbe3
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-08 14:58:09 +0800

    Add files via upload

--- pocs/middleware/apache/main.py
+++ pocs/middleware/apache/main.py
@@ -1,8 +1,9 @@
 from pocs.middleware.apache.CVE_2021_41773 import CVE_2021_41773
+from pocs.middleware.apache.CVE_2017_15715 import CVE_2017_15715
 
 def apache(url):          # 返回poc检测函数字符串列表
     poclist = [
         'CVE_2021_41773("{0}")'.format(url),
-
+        'CVE_2017_15715("{0}")'.format(url),
     ]
     return poclist

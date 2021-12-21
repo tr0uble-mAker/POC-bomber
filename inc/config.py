5572ea641a0c7c8c7f27d05a7310ff9e7c161a73
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-21 22:45:54 +0800

    Update config.py

--- inc/config.py
+++ inc/config.py
@@ -12,14 +12,17 @@ thread_timeout = 15
 
 
 ## Dnslog 配置(用于检测无回显rce)
-# dnslog获取域名(默认使用 dnslog.cn 检测)
+# dnslog 默认等待时间
+dnslog_timesleep = 8
+# 获取随机域名(默认使用 dnslog.cn 检测)
 dnslog_getdomain_url = 'http://www.dnslog.cn/getdomain.php'
-# dnslog获取响应
+# 获取 dnslog 记录响应
 dnslog_getrep_url = 'http://www.dnslog.cn/getrecords.php'
 
-# ceye配置(如果使用ceye将ceye参数置为True, 并配置域名和api)
+# ceye 配置(如果使用ceye在配置完参数后将ceye参数置为True)
 ceye = False
-# 配置ceye域名
+# 配置 ceye 域名
 ceye_domain = ''
-# 配置api参数
+# 配置 api 参数
 ceye_api = ''
+

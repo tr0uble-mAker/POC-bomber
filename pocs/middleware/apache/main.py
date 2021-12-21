9b26306791aac51d467239830540546eb0646068
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-21 21:51:50 +0800

    Update main.py

--- pocs/middleware/apache/main.py
+++ pocs/middleware/apache/main.py
@@ -1,9 +1,12 @@
 from pocs.middleware.apache.CVE_2021_41773 import CVE_2021_41773
 from pocs.middleware.apache.CVE_2017_15715 import CVE_2017_15715
+from pocs.middleware.apache.log4j2_rce import log4j2_rce
 
 def apache(url):          # 返回poc检测函数字符串列表
     poclist = [
+        'log4j2_rce("{0}")'.format(url),
         'CVE_2021_41773("{0}")'.format(url),
         'CVE_2017_15715("{0}")'.format(url),
+
     ]
     return poclist

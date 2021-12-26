8881436994679766147177ef18f5dcbee19fb1b7
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-26 14:37:27 +0800

    Update poc_bomber.py

--- poc_bomber.py
+++ poc_bomber.py
@@ -1,28 +1,51 @@
 #!/usr/bin/env python
+#############################
+#    Author tr0uble_mAker   #
+#############################
 # coding=utf-8
 
-from inc import run, output, config
+from inc import run, init, output, config
+import argparse, sys, time
+
+def get_parser():
+    parser = argparse.ArgumentParser(usage='python3 pocbomber.py -u http://xxxx -o report.txt',
+                                     description='POC Bomber: 基于python3的poc验证框架',
+                                     )
+    p = parser.add_argument_group('POC-Bomber 的参数')
+    p.add_argument("-u", "--url", type=str, help="测试单条url")
+    p.add_argument("-f", "--file", type=str, help="测试多个url文件")
+    p.add_argument("-o", "--output", help="报告生成路径(默认不生成报告)")
+    args = parser.parse_args()
+    return args
+
+def get_time():
+    return time.strftime("@ %Y-%m-%d /%H:%M:%S/", time.localtime())
 
 def main():
     output.logo()
-    choice = input(" 是否进行单URL检测(Y/n)?")
-    if choice == 'n':
-        path = input('\n 输入目标路径 >')
-        thread_num = input('\n 输入线程数量(建议线程数量20-30) >')
-        file = open(path, 'r')
-        for url in file.readlines():
-            url = url.replace('\n', '')
-            report = run.run(url, int(thread_num))
-            output.output(report, config.report_path)
-        file.close()
+    args = get_parser()
+    target_list = []
+    if args.output:
+        config.output_path = args.output
+    if args.url and args.file is None:
+        target_list.append(args.url)
+    elif args.file and args.url is None:
+        for target in open(args.file, 'r').readlines():
+            target_list.append(target)
     else:
-        url = input('\n 输入目标URL >')
-        report = run.run(url, config.thread_num_single)
-        output.output(report, config.report_path)
+        output.usage()
+        sys.exit()
+
+    print('\n[*] starting {0}\n'.format(get_time()))
+    poc_list = init.get_poc_list()
+    print('[INFO] 检测到 {0} 个目标, 已加载 {1} 条POC'.format(len(target_list), len(poc_list)))
+    if run.run(target_list, poc_list, config.output_path):
+        print('\n[+] ending {0}\n'.format(get_time()))
+    else:
+        print('\n[-] ERROR! 程序异常终止!')
+        sys.exit()
+
 
 
 if __name__ == '__main__':
     main()
-
-
-

2e83f19a03ee97d671c122814ab2a15ebae25b0a
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-27 23:42:46 +0800

    Delete pocs directory

--- pocs/ports/main.py
+++
@@ -1,9 +0,0 @@
-from pocs.ports.redis_6379 import redis_6379
-from pocs.ports.mssql_weakpasswd_1433 import mssql_weakpasswd_1443
-
-def ports():          # 返回poc检测函数字符串列表
-    poclist = [
-        'redis_6379',
-
-    ]
-    return poclist

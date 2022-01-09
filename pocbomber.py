4ed40637104fa9ab62fcacb07dffcc1747dc23f2
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2022-01-09 19:07:18 +0800

    Update pocbomber.py

--- pocbomber.py
+++ pocbomber.py
@@ -18,6 +18,7 @@ def get_parser():
     p.add_argument("-o", "--output", type=str, help="报告生成路径(默认不生成报告)")
     p.add_argument("-p", "--poc", type=str, help='指定单个或多个poc进行检测, 直接传入poc文件名, 多个poc用(,)分开')
     p.add_argument("-t", "--thread", type=int, help="指定线程池最大并发数量")
+    p.add_argument("--show", action='store_true', help="列所有出poc/exp的详细信息")
     p.add_argument("--attack", action='store_true', help="使用poc文件中的exp进行攻击")
     p.add_argument("--dnslog", action='store_true', help="使用dnslog平台检测无回显漏洞")
     args = parser.parse_args()

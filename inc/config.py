75c6105f27d878fd234dd26c6d3a7c7bed5e7667
badc0de <71172892+tr0uble-mAker@users.noreply.github.com>
2023-06-09 21:05:54 +0800

    红队版本 poc-bomber v3.0.0 redteam

--- inc/config.py
+++ inc/config.py
@@ -15,7 +15,7 @@ delay = 0
 
 ## Dnslog 配置(需要在vps上启动poc-bomber的dnslog服务端)
 # 自己购买域名设置的ns记录(对应a记录指向vps)
-dnslog_base_domain = "ns1.cdn-baidu.makeup"
+dnslog_base_domain = ""
 # dnslog是否需要认证
 dnslog_is_auth = True
 # 如需认证,在此配置密码
@@ -23,7 +23,7 @@ dnslog_auth_token = "123456"
 # dnslog http服务端口
 dnslog_web_port = 5000
 # dnslog服务端ip
-dnslog_server_ip = "120.48.87.127"
+dnslog_server_ip = ""
 
 
 
